af120337089db460ff6f0ca270fb87f91e35208e
Marcos Filomena <marcos.filomena7@gmail.com>
2023-10-05 21:17:32 -0300

    Modulo 3

---
+++ Modulo 3/funciones.js
@@ -0,0 +1,23 @@
+//  function suma3(x) {
+//    console.log(x + 3);
+//}
+
+//console.log(suma3(5));
+
+//Primer manera
+
+function suma3(x) {
+   return x + 3;
+};
+
+//Segunda manera
+
+var suma3 = function (x) {
+    return x + 3;
+};
+
+//Tercera 
+
+var suma3 = (x) => {
+    return x + 3;
+};
---
+++ Modulo 3/object_Math.js
@@ -0,0 +1,16 @@
+//Potencia (Numero, elevado)
+console.log(Math.pow(2, 3));
+
+//Redondear
+console.log(Math.round(0,51)); 
+console.log(Math.floor(5,93)); 
+console.log(Math.ceil(4,27)); 
+
+//Maximo y minimo
+
+console.log(Math.max(1,2,3,4,5));
+console.log(Math.min(1,2,3,4,5));
+
+//Numero random
+
+console.log(Math.random());
